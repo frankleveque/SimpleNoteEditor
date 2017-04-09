efb8d9942a9f4cf862dad6bec4c4f206cb5c0f09
frankleveque <leveque.frank@gmail.com>
2017-04-08 17:24:47 -0700

    add fixes and tweaks

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,6 +16,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     textFile = NULL;
 
+    //create placeholder file on startup
     if ((!QDir(dataFolder).exists()) | (QDir(dataFolder).entryList(QDir::Files).size() == 0)){
         QDir().mkdir(dataFolder);
         QFile *firstFile = new QFile("Data/placeholder.txt");
@@ -26,56 +27,147 @@ MainWindow::MainWindow(QWidget *parent) :
         delete firstFile;
     }
 
+    //set up filesystem model
     QFileSystemModel *model = new QFileSystemModel();
     model->setRootPath(QDir::currentPath());
     model->setReadOnly(false);
 
+    //attach it to listview
     ui->listView->setModel(model);
     ui->listView->setRootIndex(model->index("Data"));
-    ui->listView->setEditTriggers(QAbstractItemView::SelectedClicked);
+    ui->listView->setEditTriggers(QAbstractItemView::EditKeyPressed|QAbstractItemView::SelectedClicked);
+
+    //change to data folder
+    QDir::setCurrent("Data");
+
+    //create context menu for listview
     contextMenu = new QMenu();
 
     newFileAct = new QAction(tr("&New"), this);
     newFileAct->setStatusTip("Create New .txt file");
 
+    renameFileAct = new QAction(tr("&Rename"), this);
+    renameFileAct->setStatusTip("Rename file");
+
     delFileAct = new QAction(tr("&Delete"), this);
     delFileAct->setStatusTip("Delete selected file");
 
+    //attach actions to context
     contextMenu->addAction(newFileAct);
+    contextMenu->addAction(renameFileAct);
     contextMenu->addAction(delFileAct);
 
+    //connect signals and slots
     connect(ui->listView, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(showContextMenu(QPoint)));
     connect(newFileAct, SIGNAL(triggered(bool)), this, SLOT(newFile()));
+    connect(renameFileAct, SIGNAL(triggered(bool)), this, SLOT(startRename()));
     connect(delFileAct, SIGNAL(triggered(bool)), this, SLOT(deleteFile()));
+    connect(model, SIGNAL(fileRenamed(QString,QString,QString)), this, SLOT(reset()));
 
-    QDir::setCurrent("Data");
-
-    QList<int> startingSize;
-    startingSize << 150 << 500;
-    ui->splitter->setSizes(startingSize);
+    //set initial pane sizes
+    ui->splitter->setSizes(QList<int>({leftWidth, rightWidth}));
     ui->splitter->setStretchFactor(0,0);
 
+    reset();
+
+}
+
+void MainWindow::reset(){
+
+    textFile = NULL;
+
+    ui->plainTextEdit->blockSignals(true);
+    ui->plainTextEdit->clear();
+    ui->plainTextEdit->setReadOnly(true);
+    ui->plainTextEdit->setStyleSheet("background: #DADADA");
+
+    ui->listView->clearSelection();
+}
+
+
+void MainWindow::on_listView_doubleClicked(const QModelIndex &index)
+{
+    ui->plainTextEdit->blockSignals(true);
+    ui->plainTextEdit->clear();
+    ui->plainTextEdit->setReadOnly(false);
+    ui->plainTextEdit->setStyleSheet("");
+
+    //open selected file
+    textFile = new QFile(ui->listView->model()->data(index).toString());
+    textFile->open(QIODevice::ReadOnly | QIODevice::Text);
+
+    //populate plaintext with file contents
+    ui->plainTextEdit->setPlainText(QString::fromStdString(textFile->readAll().toStdString()));
+
+    textFile->close();
+
+    //allow plaintext to signal
+    ui->plainTextEdit->blockSignals(false);
+
+    //show indefinite status
+    QString openMsg = QString("Opened ") + QString(ui->listView->model()->data(index).toString());
+    ui->statusBar->showMessage(tr(openMsg.toStdString().c_str()));
+
+}
+
+void MainWindow::on_plainTextEdit_textChanged()
+{
+
+    if(textFile){
+        QString ptContents = ui->plainTextEdit->toPlainText();
+
+        textFile->open(QIODevice::WriteOnly);
+        textFile->write(ptContents.toStdString().c_str());
+        textFile->flush();
+        textFile->close();
+
+        //deselect file
+        ui->listView->clearSelection();
+
+        //show indefinite status
+        QString saveMsg = QString("Saved to ") + QString(textFile->fileName());
+        ui->statusBar->showMessage(tr(saveMsg.toStdString().c_str()));
+
+    }
+}
+
+void MainWindow::showContextMenu(const QPoint& pos)
+{
+    QPoint globalPos = this->mapToGlobal(pos);
+    contextMenu->exec(globalPos);
+}
+
+
+void MainWindow::newFile()
+{
+    QString newFilename = QString::number(QDateTime::currentDateTime().toMSecsSinceEpoch()) + QString(".txt");
+
+    QFile newFile(newFilename);
+    newFile.open(QIODevice::ReadWrite);
+    newFile.write("");
+    newFile.close();
+
+    qDebug() << "File created" << newFilename;
+
+}
+
+void MainWindow::startRename()
+{
+    ui->listView->edit(ui->listView->currentIndex());
 }
 
 void MainWindow::deleteFile()
 {
     QFile toRemove(ui->listView->model()->data(ui->listView->currentIndex()).toString());
     toRemove.open(QIODevice::ReadWrite);
-    qDebug() << toRemove.remove();
+    if(toRemove.remove()){
+        qDebug() << "File deleted" << toRemove.fileName();
+    }
     toRemove.close();
+
+    this->reset();
 }
 
-void MainWindow::newFile()
-{
-    QString newFilename = QString::number(QDateTime::currentDateTime().toMSecsSinceEpoch()) + QString(".txt");
-    qDebug() << newFilename;
-
-    QFile newFile(newFilename);
-    newFile.open(QIODevice::ReadWrite);
-    newFile.write("");
-    newFile.flush();
-    newFile.close();
-}
 
 
 MainWindow::~MainWindow()
@@ -87,43 +179,4 @@ MainWindow::~MainWindow()
     delete delFileAct;
 }
 
-void MainWindow::on_plainTextEdit_textChanged()
-{
-    if(textFile){
-        QString fileContents = ui->plainTextEdit->toPlainText();
-        QString saveMsg = QString("Saved to ") + QString(textFile->fileName());
-        ui->statusBar->showMessage(tr(saveMsg.toStdString().c_str()), 2500);
-        textFile->close();
-        textFile->open(QIODevice::WriteOnly);
-        textFile->write(fileContents.toStdString().c_str());
-        textFile->flush();
-        textFile->close();
-    } else{
 
-    }
-}
-
-void MainWindow::on_listView_doubleClicked(const QModelIndex &index)
-{
-    QString openMsg = QString("Opened ") + QString(ui->listView->model()->data(index).toString());
-    ui->statusBar->showMessage(tr(openMsg.toStdString().c_str()), 2500);
-
-    ui->plainTextEdit->blockSignals(true);
-
-    textFile = new QFile(ui->listView->model()->data(index).toString());
-
-    textFile->open(QIODevice::ReadOnly | QIODevice::Text);
-    ui->plainTextEdit->setPlainText(QString::fromStdString(textFile->readAll().toStdString()));
-
-    ui->plainTextEdit->blockSignals(false);
-    textFile->close();
-}
-
-void MainWindow::showContextMenu(const QPoint& pos)
-{
-    QPoint globalPos = this->mapToGlobal(pos);
-    QAction* selectedItem = contextMenu->exec(globalPos);
-    if (selectedItem){
-
-    }
-}
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QFile>
 #include <QMenu>
+#include <QModelIndex>
 
 namespace Ui {
     class MainWindow;
@@ -18,8 +19,11 @@ class MainWindow : public QMainWindow
         QFile *textFile;
         QMenu *contextMenu;
         QAction *newFileAct;
+        QAction *renameFileAct;
         QAction *delFileAct;
         const QString dataFolder = "Data";
+        const int leftWidth = 200;
+        const int rightWidth = 500;
 
     public:
         explicit MainWindow(QWidget *parent = 0);
@@ -30,8 +34,9 @@ class MainWindow : public QMainWindow
         void on_listView_doubleClicked(const QModelIndex &index);
         void showContextMenu(const QPoint&);
         void newFile();
+        void startRename();
         void deleteFile();
-
-};
+        void reset();
+ };
 
 #endif // MAINWINDOW_H
